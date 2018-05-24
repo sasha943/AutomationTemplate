7896799eae362ccee4e8cd69d2d2b510c008ddc7
oleksandr.maslovskyj <oleksandr.maslovskyj@worldapp.com>
2018-05-24 09:39:30 +0300

    remove restapi dep

--- src/test/java/tests/ui/Demo.java
+++ src/test/java/tests/ui/Demo.java
@@ -1,5 +1,6 @@
 package tests.ui;
 
+import com.qa.config.AppConfig;
 import com.qa.tests.BasicTestsExecutor;
 import com.qa.utils.SysUtils;
 import org.testng.annotations.Test;
@@ -8,7 +9,7 @@ public class Demo extends BasicTestsExecutor {
 
     @Test
     public void testThatGooglePageCanBeLoaded(){
-        webDriver.get("http://google.com");
+        webDriver.get( AppConfig.getStartUrl());
         SysUtils.sleep(6000);
     }
 }
